0a433f95f6e21f7834cd15a7ef8af93851a1b9c0
Siyabonga Zunguza <69678930+Bonga50@users.noreply.github.com>
2023-05-25 13:49:49 +0000

    Created Crud methods for USER AND PRODUCT

--- PROG3A_POE/Data/ApplicationDBContext.cs
+++ PROG3A_POE/Data/ApplicationDBContext.cs
@@ -17,11 +17,11 @@ namespace PROG3A_POE.Data
             con = _config.GetConnectionString("dbConnect");
         }
         //methed t get list of farmers 
-        public List<User> GetAllFarmers() {
+        public List<User> GetAllDistinctUsers(string UserRole) {
         
             List<User> listFarmers = new List<User>();
             SqlConnection myconnection = new SqlConnection(con);
-            SqlDataAdapter cmdSelect = new SqlDataAdapter($"select * from ApplicationUser where UserRole = 'Farmer'", myconnection);
+            SqlDataAdapter cmdSelect = new SqlDataAdapter($"select * from ApplicationUser where UserRole = '{UserRole}'", myconnection);
             DataTable dt = new DataTable();
             DataRow dr;
 
@@ -46,14 +46,96 @@ namespace PROG3A_POE.Data
             return listFarmers;
         
         }
-        // adding a new farmer to database
-        public void AddFarmer(User Farmer) {
+        // adding a new User
+        public void AddUser(User newUser) {
             using (SqlConnection myconnection = new SqlConnection(con))
             {
-                SqlCommand sqlCommand = new SqlCommand($"insert into ApplicationUser values ('{Farmer.Username}','{Farmer.Name}','{Farmer.Password}','{Farmer.User_Roles}');", myconnection);
+                SqlCommand sqlCommand = new SqlCommand($"insert into ApplicationUser values ('{newUser.Username}','{newUser.Name}','{newUser.Password}','{newUser.User_Roles}');", myconnection);
                 myconnection.Open();
                 sqlCommand.ExecuteNonQuery();
             }
+        }
+        //read User
+        public User getUser(User CertainUser) {
+            User myUser = new User();
+
+            SqlConnection myconnection = new SqlConnection(con);
+            SqlDataAdapter cmdSelect = new SqlDataAdapter($"select * from ApplicationUser where UserId = '{CertainUser.Username}'", myconnection);
+            DataTable dt = new DataTable();
+            DataRow dr;
+
+            myconnection.Open();
+            cmdSelect.Fill(dt);
+
+            if (dt.Rows.Count > 0)
+            {
+
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    dr = dt.Rows[i];
+                    myUser= new User(
+                        (string)dr["FirstName"],
+                        (string)dr["UserId"],
+                        (string)dr["Password"],
+                        (string)dr["UserRole"] );
+                }
+            }
+
+            return myUser;
+        }
+        //Update User
+        public void UpdateUser(User CertainUser) {
+
+            using (SqlConnection myconnection = new SqlConnection(con))
+            {
+                SqlCommand sqlCommand = new SqlCommand($"UPDATE ApplicationUser SET FirstName = '{CertainUser.Name}', Password = '{CertainUser.Password}', UserRole = '{CertainUser.User_Roles}' WHERE UserId = '{CertainUser.Username}';", myconnection);
+                myconnection.Open();
+                sqlCommand.ExecuteNonQuery();
+            }
+
+        }
+
+        //Delete User
+        public void DeleteUser(string UserId) {
+            using (SqlConnection myconnection = new SqlConnection(con))
+            {
+                SqlCommand sqlCommand = new SqlCommand($"DELETE FROM ApplicationUser WHERE UserId = '{UserId}';", myconnection);
+                myconnection.Open();
+                sqlCommand.ExecuteNonQuery();
+            }
+
+        }
+        //Create Product
+
+        public void createProduct(Product newProduct) {
+            using (SqlConnection myconnection = new SqlConnection(con))
+            {
+                SqlCommand sqlCommand = new SqlCommand($"INSERT INTO Products ( ProductName, ProductType, ProductDate, Quantity) " +
+                    $"VALUES ( '{newProduct.ProductName}', '{newProduct.ProductType}', '{newProduct.ProductDate}', {newProduct.Quantity});", myconnection);
+                myconnection.Open();
+                sqlCommand.ExecuteNonQuery();
+            }
+        }
+
+        //update product
+        public void updateProduct(Product CertainProduct) {
+            using (SqlConnection myconnection = new SqlConnection(con))
+            {
+                SqlCommand sqlCommand = new SqlCommand($"UPDATE Products SET ProductName = '{CertainProduct.ProductName}',ProductType = '{CertainProduct.ProductType}', ProductDate = '{CertainProduct.ProductDate}', quantity = {CertainProduct.Quantity}   WHERE ProductID = {CertainProduct.ProductID};", myconnection);
+                myconnection.Open();
+                sqlCommand.ExecuteNonQuery();
+            }
+        }
+        //delete product
+        public void deleteProduct(Product CertainProduct)
+        {
+            using (SqlConnection myconnection = new SqlConnection(con))
+            {
+                SqlCommand sqlCommand = new SqlCommand($"DELETE FROM Products WHERE ProductID = {CertainProduct.ProductID};", myconnection);
+                myconnection.Open();
+                sqlCommand.ExecuteNonQuery();
+            }
+
         }
         //getting a list of products for a specific farmer
         public List<Product> GetProducts(string UserId) {
